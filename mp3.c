1359d82dfe982e9ac204820d28f2a83215dc8c70
David <lipowic2@illinois.edu>
2016-04-06 18:07:23 -0500

    Cleaned up code, tested eveerything and added a few comments. Everything seems to be working.

--- mp3.c
+++ mp3.c
@@ -46,7 +46,7 @@ static DEFINE_MUTEX(pcb_list_mutex);
 typedef struct mp3_pcb_struct {
     struct list_head pcb_list_node;
     struct task_struct *task;
-    unsigned int pid; // TODO: maybe
+    unsigned int pid;
 
     unsigned long process_utilization;
     unsigned long major_fault_count;
@@ -57,7 +57,6 @@ typedef struct mp3_pcb_struct {
 
 typedef struct mp3_sample_struct {
 
-    // struct list_head mem_list_node;
     unsigned long jffs;
     unsigned long major_faults;
     unsigned long minor_faults;
@@ -88,12 +87,14 @@ static int chrdev_mmap(struct file *f, struct vm_area_struct *vma)
     unsigned long vm_head = vma->vm_start;
     for (i = 0; i < MP3_MEMORY_BUFFER_PAGE_COUNT; i++) {
         int offset = i * MP3_PAGE_SIZE;
+	
+	// Declare and initialize virtual addresses
 
         void * page_virtual_address = memory_buffer + offset;
         unsigned long user_virtual_address = vm_head + offset;
 
         unsigned long pfn = vmalloc_to_pfn(page_virtual_address);
-        remap_pfn_range(vma, user_virtual_address, pfn, MP3_PAGE_SIZE, vma->vm_page_prot);
+        remap_pfn_range(vma, user_virtual_address, pfn, MP3_PAGE_SIZE, vma->vm_page_prot);	// Call the remap to remap the virtual address
     }
 
     return 0;
@@ -106,16 +107,6 @@ static struct file_operations chrdev_fops = {
 static int chrdev_major;
 /* end character device */
 
-/*
-static void debug_print_list(void) {
-
-    mp3_pcb *i; /
-    printk(KERN_ALERT "I EXIST\n");
-    list_for_each_entry(i, &pcb_list_head, pcb_list_node) {
-        printk(KERN_ALERT "Have pid: %u\n", i->pid);
-    }
-}
-*/
 static void wq_fun(struct work_struct *mp3_work) {
     mp3_pcb *i;
 
@@ -134,30 +125,25 @@ static void wq_fun(struct work_struct *mp3_work) {
             unsigned long j_utime;
             unsigned long j_stime;
 
-            get_cpu_use(i->pid, &(maj_flt), &(min_flt), &(utime), &(stime));
+            get_cpu_use(i->pid, &(maj_flt), &(min_flt), &(utime), &(stime));  // Get the data for profiling the page faults
 
             major_faults += maj_flt;
             minor_faults += min_flt;
 
-            // ask whether to divide by jiffies or jffs?
-            // TODO: this is wrong! it always does 0
-            
-
             j_utime = cputime_to_jiffies(utime); 
             j_stime = cputime_to_jiffies(stime);
 
-            printk(KERN_ALERT "utime:%lu\nstime:%lu\nj_utime:%lu\nj_stime:%lu\n", utime, stime, j_utime, j_stime);
-            cpu_util += (j_stime + j_utime);
+            printk(KERN_ALERT "utime:%lu\nstime:%lu\nj_utime:%lu\nj_stime:%lu\n, major faults: %lu\n, minor faults: %lu\n", utime, stime, j_utime, j_stime, major_faults, minor_faults);
+            cpu_util += (j_stime + j_utime);		// Calculate the cpu utilization time
         }
+
+	// Store obtained sample	
 
         sample->jffs = jffs;
         sample->major_faults = major_faults;
         sample->minor_faults = minor_faults;
         sample->cpu_util = cpu_util;
-        // printk(KERN_ALERT "Logging sample: %lu\nmaj: %lu\nmin: %lu\ncpu: %lu\nwrite_idx:%d\nread_idx:%d\n", jffs, major_faults, minor_faults, cpu_util, write_idx, read_idx);
 
-        // they point to the same spot because the queue is full
-        // advance both pointers and don't update the quantity
         if (write_idx == read_idx && quantity_unread == QUEUE_SIZE) {
                 read_idx = (read_idx + 1) % QUEUE_SIZE;
                 write_idx = (write_idx + 1) % QUEUE_SIZE;
@@ -165,6 +151,8 @@ static void wq_fun(struct work_struct *mp3_work) {
             write_idx = (write_idx + 1) % QUEUE_SIZE;
             quantity_unread++;
         }
+	
+	// Create the workqueue job
 
         create_wq_job();
     }
@@ -177,61 +165,35 @@ static void create_wq_job(void) {
     queue_delayed_work(mp3_wq, &mp3_work, msecs_to_jiffies(50));
 }
 
-// // helpers for timer code
-// static struct timer_list myTimer;
-// void timerFun (unsigned long arg) {
-//     myTimer.expires = jiffies + 5 * HZ;
-//     add_timer (&myTimer); /* setup the timer again */
-    
-
-
-//     // schedule_work(&mp3_wq); // trigger the bottom half
-//     // 
-    
-    
-//     // create_wq_job();
-// }
-
-
-
-    
 
 static void REGISTER(unsigned int pid) {
     mp3_pcb *pcb;
     struct task_struct *task;
-    //unsigned long currentTime;
-    //unsigned long expiryTime;
 
     printk(KERN_ALERT "REGISTER called for pid: %u\n", pid);
-    task = find_task_by_pid(pid);
+    task = find_task_by_pid(pid);		// Find the required task
 
-    // pid not valid
     if (task == NULL) { 
         printk(KERN_ALERT "find_task_by_pid failed\n");
         return;
     }
     
+    // Allocate and fill in the pcb
+
     pcb = kmalloc(sizeof(mp3_pcb), GFP_KERNEL);
     pcb->task = task;
     pcb->pid = pid;
 
     mutex_lock(&pcb_list_mutex);
     if (pcb_num_elements == 0) {
-        // currentTime = jiffies; // pre-defined kernel variable jiffies gives current value of ticks
-        // expiryTime = currentTime + 5 * HZ; 
-        // init_timer (&myTimer);
-        // myTimer.function = timerFun;
-        // myTimer.expires = expiryTime;
-        // myTimer.data = 0;
-        // add_timer (&myTimer);
         create_wq_job();
     }
+
+    // Add to the list
+
     list_add(&(pcb->pcb_list_node), &pcb_list_head);
     pcb_num_elements++;
     mutex_unlock(&pcb_list_mutex);
-
-    // debug_print_list(); //Function is commented out above because it throws up a warning. Uncomment to use.
-
 
     printk(KERN_ALERT "REGISTER successful!\n");
 }
@@ -241,35 +203,24 @@ static void UNREGISTER(unsigned int pid) {
     mp3_pcb *next;
     
     printk(KERN_ALERT "UNREGISTER called for pid: %u\n", pid);
-    // printk(KERN_ALERT "line 136\n");
     mutex_lock(&pcb_list_mutex);
-    // printk(KERN_ALERT "line 138\n");
+
+    // Iterate through list and unregister and delete all entries
+
     list_for_each_entry_safe(i, next, &pcb_list_head, pcb_list_node) {
-        // printk(KERN_ALERT "line 140\n");
         if (i->pid == pid) {
-            // printk(KERN_ALERT "line 142\n");
             list_del(&(i->pcb_list_node));
-            // printk(KERN_ALERT "line 144\n");
             kfree(i);
-            // printk(KERN_ALERT "line 146\n");
             pcb_num_elements--;
-            // printk(KERN_ALERT "line 148\n");
         }
     }
 
-    // printk(KERN_ALERT "line 152\n");
     if (pcb_num_elements == 0) {
-        // printk(KERN_ALERT "line 154\n");
         // del_timer(&myTimer);
-        // printk(KERN_ALERT "line 156\n");
         
-        // printk(KERN_ALERT "line 158\n");
         // mp3_wq = NULL;
     }   
-    // printk(KERN_ALERT "line 161\n");
     mutex_unlock(&pcb_list_mutex);
-    // printk(KERN_ALERT "line 163\n");
-    // debug_print_list(); //Function is commented out above because it throws up a warning. Uncomment to use.
 
 }
 
@@ -328,8 +279,6 @@ static ssize_t mp3_write (struct file *file, const char __user *buffer, size_t c
 
     printk(KERN_ALERT "mp3_write called\n");
 
-    // unsigned long period;
-    // unsigned long cputime;
     
     // manually null terminate
     buf = (char *) kmalloc(count+1,GFP_KERNEL); 
